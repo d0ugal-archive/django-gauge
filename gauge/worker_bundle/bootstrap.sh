9954b0a032dbafcb4c07357d7f44e2da5a131d6e
Dougal Matthews <dougal85@gmail.com>
2011-12-30 13:52:24 +0000

    Use the pip bootstrap to remove the need for easy install

--- gauge/worker_bundle/bootstrap.sh
+++ gauge/worker_bundle/bootstrap.sh
@@ -1,7 +1,5 @@
 export PIP_USE_MIRRORS=true;
 export PIP_FIND_LINKS="http://pypi.python.org http://d.pypi.python.org";
-sudo apt-get install python-dev python-setuptools git-core -y -q;
-echo "installing pip";
-sudo easy_install pip;
-echo "pip installed";
+sudo apt-get install python-dev python-setuptools git-core mercurial -y -q;
+sudo curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | sudo python
 sudo pip install -q -r ~/gauge/requirements.txt;
