3900cdcb83fa9cc5736854b12db53e29a50a3211
Dougal Matthews <dougal85@gmail.com>
2012-01-01 14:03:27 +0000

    Added moar pip mirrors.

--- gauge/worker_bundle/bootstrap.sh
+++ gauge/worker_bundle/bootstrap.sh
@@ -1,5 +1,7 @@
 export PIP_USE_MIRRORS=true;
-export PIP_FIND_LINKS="http://pypi.python.org http://d.pypi.python.org";
+export PIP_FIND_LINKS="http://pypi.python.org b.pypi.python.org c.pypi.python.org d.pypi.python.org e.pypi.python.org f.pypi.python.org";
 sudo apt-get install python-dev python-setuptools git-core mercurial -y -q;
-sudo curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | sudo python
+sudo curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | sudo python;
 sudo pip install -q -r ~/gauge/requirements.txt;
+
+
